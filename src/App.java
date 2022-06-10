49fe165967f82bf1185a4fed41277a987905cec7
Damar Albaribin <damaralbaribin@gmail.com>
2022-06-10 17:02:01 +0700

    feat : working lexer

--- src/App.java
+++ src/App.java
@@ -1,6 +1,17 @@
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
+        String text = "1 + 2 * 3 - 4 / 5";
+
+        Lexer lexer = new Lexer(text);
+
+        List<Token> tokens = lexer.generateTokens();
+
+        for (Token token : tokens) {
+            System.out.printf("v: %s type : %s  \n", token.value, token.type);
+        }
     }
 
 }
--- src/Lexer.java
+++ src/Lexer.java
@@ -1,12 +1,71 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Lexer {
     public final String DIGITS = "0123456789";
     public final String WHITESPACE = "\n\t\r ";
 
-    public int current = 0;
+    public char current;
 
     public String text;
 
+    public int pos = 0;
+
     Lexer(String text) {
         this.text = text;
+        advance();
+    }
+
+    void advance() {
+        try {
+            current = text.charAt(pos);
+            pos++;
+        } catch (IndexOutOfBoundsException e) {
+            current = '\0';
+        }
+    }
+
+    List<Token> generateTokens() {
+
+        List<Token> tokens = new ArrayList<>();
+
+        while (current != '\0') {
+
+            if (current == ' ' || current == '\n' || current == '\t') {
+                advance();
+            }
+            if (current == '+') {
+                tokens.add(new Token(TokenType.PLUS, "+"));
+                advance();
+            } else if (current == '-') {
+                tokens.add(new Token(TokenType.MINUS, "-"));
+                advance();
+            } else if (current == '*') {
+                tokens.add(new Token(TokenType.MULTPIPLY, "*"));
+                advance();
+            } else if (current == '/') {
+                tokens.add(new Token(TokenType.DIVIDE, "/"));
+                advance();
+            } else if (current == '(') {
+                tokens.add(new Token(TokenType.LPAREN, "("));
+                advance();
+            } else if (current == ')') {
+                tokens.add(new Token(TokenType.RPAREN, ")"));
+                advance();
+            } else if (Character.isDigit(current)) {
+                StringBuilder sb = new StringBuilder();
+                while (Character.isDigit(current)) {
+                    sb.append(current);
+                    advance();
+                }
+                tokens.add(new Token(TokenType.NUMBER, sb.toString()));
+            } else {
+                throw new RuntimeException("Invalid character: " + current);
+            }
+        }
+
+        return tokens;
+
     }
 }
