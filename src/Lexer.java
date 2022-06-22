42774974a2da2b6843640e1b5a840d5d736a96f8
Damar Albaribin <damaralbaribin@gmail.com>
2022-06-22 10:24:00 +0700

    fix: any

--- src/Lexer.java
+++ src/Lexer.java
@@ -3,69 +3,71 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Lexer {
-    public final String DIGITS = "0123456789";
-    public final String WHITESPACE = "\n\t\r ";
+    public final String DIGITS = "0123456789"; // Digit
+    public final String WHITESPACE = "\n\t\r "; // Whitespace
 
-    public char current;
+    public char current; // Digunakan untuk menyimpan karakter yang sedang dibaca
 
-    public String text;
+    public String text; // Digunakan untuk menyimpan input dari user
 
-    public int pos = 0;
+    public int pos = 0; // Digunakan untuk menyimpan posisi dari karakter yang sedang dibaca
 
-    Lexer(String text) {
-        this.text = text;
-        advance();
+    Lexer(String text) { // Konstruktor
+        this.text = text; // Menyimpan input dari user
+        advance(); // Membaca karakter pertama
     }
 
     void advance() {
         try {
-            current = text.charAt(pos);
-            pos++;
-        } catch (IndexOutOfBoundsException e) {
-            current = '\0';
+            current = text.charAt(pos); // Menyimpan karakter yang sedang dibaca
+            pos++; // Menajukan posisi ke karakter berikutnya
+        } catch (IndexOutOfBoundsException e) { // Jika posisi melebihi ukuran string
+            current = '\0'; // Menyimpan karakter null
         }
     }
 
     List<Token> generateTokens() {
 
-        List<Token> tokens = new ArrayList<>();
+        List<Token> tokens = new ArrayList<>(); // Membuat list dari token yang diterima dari Lexer
 
-        while (current != '\0') {
+        while (current != '\0') { // Selama karakter yang sedang dibaca tidak sama dengan null
 
-            if (current == ' ' || current == '\n' || current == '\t') {
-                advance();
+            if (current == ' ' || current == '\n' || current == '\t') { // Jika karakter yang sedang dibaca adalah
+                                                                        // whitespace
+                advance(); // Menajukan posisi ke karakter berikutnya
             }
-            if (current == '+') {
-                tokens.add(new Token(TokenType.PLUS, "+"));
-                advance();
-            } else if (current == '-') {
-                tokens.add(new Token(TokenType.MINUS, "-"));
-                advance();
-            } else if (current == '*') {
-                tokens.add(new Token(TokenType.MULTPIPLY, "*"));
-                advance();
-            } else if (current == '/') {
-                tokens.add(new Token(TokenType.DIVIDE, "/"));
-                advance();
-            } else if (current == '(') {
-                tokens.add(new Token(TokenType.LPAREN, "("));
-                advance();
-            } else if (current == ')') {
-                tokens.add(new Token(TokenType.RPAREN, ")"));
-                advance();
-            } else if (Character.isDigit(current)) {
-                StringBuilder sb = new StringBuilder(); // "123"
-                while (Character.isDigit(current) || current == '.') {
-                    sb.append(current);
-                    advance();
+            if (current == '+') { // Jika karakter yang sedang dibaca adalah +
+                tokens.add(new Token(TokenType.PLUS, "+")); // Menambahkan token + ke list
+                advance(); // Menajukan posisi ke karakter berikutnya
+            } else if (current == '-') { // Jika karakter yang sedang dibaca adalah -
+                tokens.add(new Token(TokenType.MINUS, "-")); // Menambahkan token - ke list
+                advance(); // Menajukan posisi ke karakter berikutnya
+            } else if (current == '*') { // Jika karakter yang sedang dibaca adalah *
+                tokens.add(new Token(TokenType.MULTPIPLY, "*")); // Menambahkan token * ke list
+                advance(); // Menajukan posisi ke karakter berikutnya
+            } else if (current == '/') { // Jika karakter yang sedang dibaca adalah /
+                tokens.add(new Token(TokenType.DIVIDE, "/")); // Menambahkan token / ke list
+                advance(); // Menajukan posisi ke karakter berikutnya
+            } else if (current == '(') { // Jika karakter yang sedang dibaca adalah (
+                tokens.add(new Token(TokenType.LPAREN, "(")); // Menambahkan token ( ke list
+                advance(); // Menajukan posisi ke karakter berikutnya
+            } else if (current == ')') { // Jika karakter yang sedang dibaca adalah )
+                tokens.add(new Token(TokenType.RPAREN, ")")); // Menambahkan token ) ke list
+                advance(); // Menajukan posisi ke karakter berikutnya
+            } else if (Character.isDigit(current)) { // Jika karakter yang sedang dibaca adalah digit
+                StringBuilder sb = new StringBuilder(); // Membuat string builder untuk nomor
+                while (Character.isDigit(current) || current == '.') { // Selama karakter yang sedang dibaca adalah
+                                                                       // digit ataupun .
+                    sb.append(current); // Menambahkan karakter yang sedang dibaca ke string builder
+                    advance(); // Menajukan posisi ke karakter berikutnya
                 }
-                tokens.add(new Token(TokenType.NUMBER, sb.toString()));
-            } else {
-                throw new RuntimeException("Invalid character: " + current);
+                tokens.add(new Token(TokenType.NUMBER, sb.toString())); // Menambahkan token nomor ke list
+            } else { // Jika karakter yang sedang dibaca bukanlah digit, whitespace, atau operator
+                throw new RuntimeException("Invalid character: " + current); // Menampilkan error
             }
         }
 
-        return tokens;
+        return tokens; // Mengembalikan list dari token yang diterima dari Lexer
 
     }
 }
